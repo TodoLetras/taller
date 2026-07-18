949dae99c174fbdeee9af8469a60ba949149e3be
TodoLetras <imgpositivaok@gmail.com>
2026-07-18 17:17:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'taller-v1';
+const ARCHIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const esNavegacion = e.request.mode === 'navigate' || e.request.destination === 'document';
+  if (esNavegacion) {
+    // red primero: si hay internet trae la última versión; si no, usa la guardada
+    e.respondWith(
+      fetch(e.request).then(r => {
+        const copia = r.clone();
+        caches.open(CACHE).then(c => c.put('./index.html', copia));
+        return r;
+      }).catch(() => caches.match('./index.html'))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
