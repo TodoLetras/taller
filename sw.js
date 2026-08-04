6904d4ee281f0bb1f84eda6b68354cdc8af7d64b
TodoLetras <imgpositivaok@gmail.com>
2026-08-03 21:19:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taller-v97';
+const CACHE = 'taller-v98';
 const ARCHIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png', './favicon.ico', './favicon-32.png', './cotizador.html', './pdf-assets.js'];
 
 self.addEventListener('install', e => {
